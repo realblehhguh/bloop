5e2616dbdefb1030f81a9e06d8846f44060ca53a
realblehhguh <iamhappyandfree@proton.me>
2025-07-11 00:04:04 -0400

    ok

--- twrp_kona.mk
+++ twrp_kona.mk
@@ -10,7 +10,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common OrangeFox stuff.
-$(call inherit-product, vendor/recovery/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from kona device
 $(call inherit-product, device/moorechip/kona/device.mk)
